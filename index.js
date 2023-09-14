91bdb0ded8f7837a75303182cb4ae679a4612a26
mwafrika <mwafrikajosue@gmail.com>
2023-09-14 18:40:05 +0300

    Fix deployment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const dotenv = require("dotenv");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+dotenv.config();
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -20,6 +22,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
